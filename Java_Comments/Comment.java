1049e629f5a640509f5320642d5ae8a27e44c869
pradeepmeena05 <pradeepmeena34330@gmail.com>
2023-10-23 22:43:43 +0530

    code added

---
+++ Java_Comments/Comment.java
@@ -0,0 +1,14 @@
+class Comment {
+     public static void main(String []args){
+        
+        // System.out.println("This is single line comment");
+
+
+      /*   System.out.println("This is multiple line comment");
+        System.out.println("This is multiple line comment");
+        System.out.println("This is multiple line comment");
+        System.out.println("This is multiple line comment");
+        System.out.println("This is multiple line comment"); */
+
+     }
+}
