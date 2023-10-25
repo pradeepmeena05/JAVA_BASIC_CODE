43ff59b6c0f6fdeac947c0df80755a3ff2f51c06
pradeepmeena05 <pradeepmeena34330@gmail.com>
2023-10-25 10:51:52 +0530

    code added

---
+++ Type_casting/Example.java
@@ -0,0 +1,15 @@
+
+public class Example {
+    
+
+    public static void main(String[] args) {
+
+         System.out.println("This is type_casting");
+        int value1 = 9;
+        float value2 = (float) value1;
+
+        System.out.println(value2);
+  
+    }
+}
+
