5858798074bc35d3209b7df175f48282e348175f
pradeepmeena05 <pradeepmeena34330@gmail.com>
2023-10-26 11:01:42 +0530

    code added

---
+++ Math/Example.java
@@ -0,0 +1,23 @@
+
+public class Example {
+    public static void main(String[] args) {
+        
+        int a=10;
+        int b=99;
+
+        System.out.println("maximum number "+Math.max(a,b)); //The Math.min(x,y) method can be used to find the lowest value of x and y:
+
+         System.out.println("minimum number "+Math.min(a,b)); //The Math.min(x,y) method can be used to find the lowest value of x and y:
+
+
+         int val=64;
+
+         System.out.println("sqrt of "+val+" = "+Math.sqrt(val)); //The Math.sqrt(x) method returns the square root of x:
+
+         System.out.println(Math.abs(-4.5)); //The Math.abs(x) method returns the absolute (positive) value of x:
+
+         int randomNum = (int)(Math.random() * 101);
+
+         System.out.println("random number="+randomNum);
+    }
+}
