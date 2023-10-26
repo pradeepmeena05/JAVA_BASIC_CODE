f03db891f649b8dc65bba9be48fdd1c16cbb6896
pradeepmeena05 <pradeepmeena34330@gmail.com>
2023-10-26 11:00:43 +0530

    code added

---
+++ IF_ELSE/Example.java
@@ -0,0 +1,18 @@
+public class Example {
+    public static void main(String args[]){
+
+        int target=20;
+
+        // if(target>10)
+        // {
+        //      System.out.println("target is greater");
+        // }
+        // else{
+        //     System.out.println("target is smallest");
+        // }
+
+        String result=target>18 ? "good day" :"good evening"; //ternary operator
+
+        System.out.println(result);
+    }
+}
