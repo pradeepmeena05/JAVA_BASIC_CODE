9787311b640eab07c145f7de4df4ccd5ac54a2db
pradeepmeena05 <pradeepmeena34330@gmail.com>
2023-10-26 11:00:16 +0530

    code added

---
+++ Break/Continue/Example.java
@@ -0,0 +1,20 @@
+public class Example {
+    public static void main(String args[]){
+
+//         for (int i = 0; i < 10; i++) {
+//             if (i == 4) {
+//               continue;
+//             }
+//             System.out.println(i);
+// }
+
+            int i = 0;
+          while (i < 10) {
+              System.out.println(i);
+              i++;
+                if (i == 4) {
+                break;
+  }
+    }
+    }
+}
