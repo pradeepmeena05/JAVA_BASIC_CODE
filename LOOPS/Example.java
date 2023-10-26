97fb70293716a66705af18156c066c390b95f653
pradeepmeena05 <pradeepmeena34330@gmail.com>
2023-10-26 11:01:10 +0530

    code added

---
+++ LOOPS/Example.java
@@ -0,0 +1,23 @@
+public class Example {
+    
+    public static void main(String args[]){
+
+        for(int i=0;i<5;i++)  //for loop
+        {
+            System.out.println("for loop"+" "+ i);
+        }
+
+        //    int i=0;
+        //    while(i<5)
+        //    {
+        //     System.out.println("while loop "+ i);
+        //     i++;
+        //    }
+
+        //   int i=0;
+        // do{
+        //     System.out.println(" do while loop "+ i);
+        //     i++;
+        // }while(i<5);
+    }
+}
