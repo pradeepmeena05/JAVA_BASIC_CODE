e58a7a0d5b2a69d6962fe1fc0fade4b57f3a9db8
pradeepmeena05 <pradeepmeena34330@gmail.com>
2023-10-27 10:03:38 +0530

    code added

---
+++ Methods/Example.java
@@ -0,0 +1,12 @@
+public class Example {
+    static void display(){
+        System.out.println("its a method");
+      }
+    
+    public static void main(String args[]){
+
+      display();// here we used static keyword we dont need to make object of class.
+    }
+  
+}
+
