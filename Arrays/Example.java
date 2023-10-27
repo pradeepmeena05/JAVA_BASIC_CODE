f79c01bbf662ccdf47c429971e858b4ab33691b3
pradeepmeena05 <pradeepmeena34330@gmail.com>
2023-10-27 10:06:42 +0530

    update

--- Arrays/Example.java
+++ Arrays/Example.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
 public class Example {
     public static void main(String args[]){
 
         String [] student={"name","age","roll.no"};
 
+       
         for(int i=0;i<student.length;i++){
             System.out.println(student[i]);
         }
@@ -11,17 +13,17 @@ public class Example {
         for(int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
         }
-
+       
         //Java Multi-Dimensional Arrays
 
-        int mularr[][]={{2,3,45,6},{66,8,2,55,}};
+        // int mularr[][]={{2,3,45,6},{66,8,2,55,}};
 
-        for(int i=0;i<mularr.length;i++)
-        {
-          for(int j=0;j<mularr[i].length;j++)
-          {
-            System.out.println(mularr[i][j]);
-          }
-        }
+        // for(int i=0;i<mularr.length;i++)
+        // {
+        //   for(int j=0;j<mularr[i].length;j++)
+        //   {
+        //     System.out.println(mularr[i][j]);
+        //   }
+        // }
     }
 }
