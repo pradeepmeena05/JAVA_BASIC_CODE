87f3073198518dfb38d03f451284b44d73ea26d6
pradeepmeena05 <pradeepmeena34330@gmail.com>
2023-10-29 18:24:34 +0530

    code added

---
+++ Final_keyword/Example.java
@@ -0,0 +1,13 @@
+public class Example {
+    
+    public static void main(String args[]){
+
+        final int value=92;
+
+      //value=89;  Re-assigning final variable
+        // will throw compile-time error
+
+        System.out.println("final can not we change "+value);
+
+    }
+}
